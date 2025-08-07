7f630dca9c1707021805eeb6b9a3dc3efc3bcd58
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2025-08-07 13:56:56 +0300

    feat: use NTT for CyclotomicRing multiplication

--- src/arith/cyclotomic_ring.rs
+++ src/arith/cyclotomic_ring.rs
@@ -1,9 +1,17 @@
-use std::iter::Sum;
+use std::{
+    iter::Sum,
+    sync::{Once, OnceLock},
+};
 
 use ark_ff::{Field, PrimeField};
 use ark_std::rand::Rng;
 
+use tfhe_ntt::prime64::Plan;
+
 use super::ring::Ring;
+
+// At what point we start using NTT for multiplication.
+const NTT_CUTOFF: usize = 32;
 
 /// Represents an element of F\[X\]/(X^N + 1).
 #[derive(Clone, Debug)]
@@ -77,6 +85,66 @@ impl<const D: usize, F: PrimeField> CyclotomicRing<D, F> {
 
         Self { coeffs }
     }
+
+    pub fn basic_mul(&self, rhs: &Self) -> Self {
+        let mut coeffs = Vec::<F>::with_capacity(D);
+        for i in 0..D {
+            let mut coeff = F::ZERO;
+            for j in 0..i + 1 {
+                coeff = coeff + (self.coeffs[j] * rhs.coeffs[i - j]);
+            }
+            for j in i + 1..D {
+                coeff = coeff - (self.coeffs[j] * rhs.coeffs[D - j + i]);
+            }
+            coeffs.push(coeff);
+        }
+        CyclotomicRing { coeffs }
+    }
+
+    // If the plan is valid for 64 bit NTT based on D and F, we use NTT. Else we use basic multiplication.
+    fn try_ntt_mul(&self, rhs: &Self) -> Self {
+        if let Some(plan) = Self::ntt_plan_64() {
+            self.ntt_mul(rhs, plan)
+        } else {
+            self.basic_mul(rhs)
+        }
+    }
+
+    fn ntt_mul(&self, rhs: &Self, plan: &Plan) -> Self {
+        // From https://github.com/zama-ai/tfhe-rs/blob/main/tfhe-ntt/examples/mul_poly_prime.rs
+        // Converting to u64 should be safe as we assume that the modulus fits in 64 bits and that's checked at NTT plan creation.
+        let mut lhs_u64 = self
+            .coeffs
+            .iter()
+            .map(|c| c.into_bigint().as_ref()[0])
+            .collect::<Vec<_>>();
+        let mut rhs_u64 = rhs
+            .coeffs
+            .iter()
+            .map(|c| c.into_bigint().as_ref()[0])
+            .collect::<Vec<_>>();
+        plan.fwd(&mut lhs_u64);
+        plan.fwd(&mut rhs_u64);
+        plan.mul_assign_normalize(&mut lhs_u64, &rhs_u64);
+        plan.inv(&mut lhs_u64);
+        let coeffs = lhs_u64.iter().map(|&x| F::from(x)).collect::<Vec<_>>();
+        CyclotomicRing { coeffs }
+    }
+
+    // Gets a static plan for the 64 bit NTT. If the polynomial size or the prime modulus are not suitable,
+    // returns None.
+    fn ntt_plan_64() -> Option<&'static Plan> {
+        static PLAN: OnceLock<Option<Plan>> = OnceLock::new();
+        PLAN.get_or_init(|| {
+            if F::MODULUS.as_ref().len() != 1 {
+                // Modulus must fit in 64 bits.
+                return None;
+            }
+            let modulus = F::MODULUS.as_ref()[0];
+            Plan::try_new(D, modulus)
+        })
+        .as_ref()
+    }
 }
 
 impl<const D: usize, F: PrimeField> Default for CyclotomicRing<D, F> {
@@ -125,20 +193,11 @@ impl<const D: usize, F: PrimeField> Ring for CyclotomicRing<D, F> {
         }
     }
 
-    // TODO: use NTT for better performances
-    fn mul(&self, rhs: &CyclotomicRing<D, F>) -> Self {
-        let mut coeffs = Vec::<F>::with_capacity(D);
-        for i in 0..D {
-            let mut coeff = F::ZERO;
-            for j in 0..i + 1 {
-                coeff = coeff + (self.coeffs[j] * rhs.coeffs[i - j]);
-            }
-            for j in i + 1..D {
-                coeff = coeff - (self.coeffs[j] * rhs.coeffs[D - j + i]);
-            }
-            coeffs.push(coeff);
+    fn mul(&self, rhs: &Self) -> Self {
+        if D <= NTT_CUTOFF {
+            return self.basic_mul(rhs);
         }
-        CyclotomicRing { coeffs }
+        self.try_ntt_mul(rhs)
     }
 
     fn zero() -> Self {
@@ -172,7 +231,11 @@ impl<const D: usize, F: PrimeField> Ring for CyclotomicRing<D, F> {
 
 #[cfg(test)]
 mod test {
-    use crate::arith::{cyclotomic_ring::CyclotomicRing, field::Field64, ring::Ring};
+    use crate::arith::{
+        cyclotomic_ring::{CyclotomicRing, NTT_CUTOFF},
+        field::Field64,
+        ring::Ring,
+    };
     use ark_ff::{AdditiveGroup, Field};
     use rand::{Rng, rng};
 
@@ -201,5 +264,67 @@ mod test {
 
             assert_eq!(res_mul.coeffs, res_monomial_mul.coeffs);
         }
+    }
+
+    #[test]
+    fn test_basic_mul() {
+        const D: usize = 4;
+        let lhs = CyclotomicRing::<D, Field64>::from_coeffs(&[
+            Field64::from(1u64),
+            Field64::from(2u64),
+            Field64::from(0u64),
+            Field64::from(3u64),
+        ]);
+
+        let rhs = CyclotomicRing::<D, Field64>::from_coeffs(&[
+            Field64::from(0u64),
+            Field64::from(1u64),
+            Field64::from(9u64),
+            Field64::from(0u64),
+        ]);
+
+        let res = lhs.basic_mul(&rhs);
+        assert_eq!(
+            res.coeffs,
+            vec![
+                Field64::from(18446744069414584318u64),
+                Field64::from(18446744069414584295u64),
+                Field64::from(11u64),
+                Field64::from(18u64)
+            ]
+        );
+    }
+
+    #[test]
+    fn test_ntt_mul() {
+        const D: usize = NTT_CUTOFF * 2;
+        let lhs = CyclotomicRing::<D, Field64>::get_random();
+        let rhs = CyclotomicRing::<D, Field64>::get_random();
+
+        let basic_res = lhs.basic_mul(&rhs);
+        let ntt_res = lhs.try_ntt_mul(&rhs);
+        assert_eq!(basic_res.coeffs, ntt_res.coeffs);
+    }
+
+    #[test]
+    fn test_mul_via_ntt() {
+        const D: usize = NTT_CUTOFF * 2;
+        let lhs = CyclotomicRing::<D, Field64>::get_random();
+        let rhs = CyclotomicRing::<D, Field64>::get_random();
+
+        let basic_res = lhs.basic_mul(&rhs);
+        let mul_res = lhs.mul(&rhs);
+        assert_eq!(basic_res.coeffs, mul_res.coeffs);
+    }
+
+    #[test]
+    fn test_mul_via_basic() {
+        const D: usize = NTT_CUTOFF / 2;
+        let lhs = CyclotomicRing::<D, Field64>::get_random();
+        let rhs = CyclotomicRing::<D, Field64>::get_random();
+
+        let basic_res = lhs.basic_mul(&rhs);
+        let mul_res = lhs.mul(&rhs);
+        assert_eq!(basic_res.coeffs, mul_res.coeffs);
     }
 }
