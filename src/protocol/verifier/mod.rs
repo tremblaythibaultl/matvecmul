7e13d4538d578fd59773c5eb173d80eac7f8fc92
tremblaythibaultl <tremblaythibaultl@gmail.com>
2025-11-13 11:40:10 -0500

    profiling

--- src/protocol/verifier/mod.rs
+++ src/protocol/verifier/mod.rs
@@ -48,26 +48,27 @@ where
         x: &Vec<RLWE<CyclotomicRing<D, F::BasePrimeField>>>,
         proof: Proof<D, F>,
     ) -> Result<Vec<F>, ()> {
-        // let start = Instant::now();
+        let start = Instant::now();
         let mut x_bytes_to_absorb = vec![];
         for ct in x.iter() {
             // consider body only (compressed ciphertext)
             let el = ct.get_ring_element(1).unwrap();
             el.serialize(&mut x_bytes_to_absorb).unwrap();
         }
+        let after_process_x = start.elapsed();
+        println!("Processing x to absorb took: {:?}", after_process_x);
+        println!("\n\n--- VERIFIER TIMINGS ---");
 
-        // println!("\n\n--- VERIFIER TIMINGS ---");
-
-        // let start = Instant::now();
+        let start = Instant::now();
         transcript.absorb_bytes_par(&x_bytes_to_absorb);
-        // let after_absorb = start.elapsed();
-        // println!("time to absorb x: {:?}", after_absorb);
+        let after_absorb = start.elapsed();
+        println!("time to absorb x: {:?}", after_absorb);
 
         let mut beta = transcript.squeeze(1);
         beta.push(beta[0] * beta[0]);
 
         // assumes RLWE rank is 1
-        // let start = Instant::now();
+        let start = Instant::now();
         let y_batched = proof
             .y
             .iter()
@@ -84,88 +85,88 @@ where
             })
             .flatten()
             .collect::<Vec<F>>();
-        // let after_batching_y = start.elapsed();
-        // println!("time to batch y: {:?}", after_batching_y);
+        let after_batching_y = start.elapsed();
+        println!("time to batch y: {:?}", after_batching_y);
 
-        // let start = Instant::now();
+        let start = Instant::now();
         let mut y_bytes_to_absorb = vec![];
         for y_batched_i in y_batched.iter() {
             y_batched_i
                 .serialize_uncompressed(&mut y_bytes_to_absorb)
                 .unwrap();
         }
-        // let processed_y = start.elapsed();
-        // println!("processing batched y took: {:?}", processed_y);
+        let processed_y = start.elapsed();
+        println!("processing batched y took: {:?}", processed_y);
 
-        // let start = Instant::now();
+        let start = Instant::now();
         transcript.absorb_bytes_par(&y_bytes_to_absorb);
-        // let after_absorb_y = start.elapsed();
-        // println!("time to absorb y : {:?}", after_absorb_y);
+        let after_absorb_y = start.elapsed();
+        println!("time to absorb y : {:?}", after_absorb_y);
 
         // TODO: split commit/prove in WHIR and absorb commitment to r too
 
         let m = m_rq.height().ilog2() as usize;
-        // let start = Instant::now();
+        let start = Instant::now();
         let mut challenges = transcript.squeeze(m + 1);
         let alpha = challenges.pop().unwrap();
         let vec_tau = challenges;
-        // let after_squeeze = start.elapsed();
-        // println!("Squeezing challenges took: {:?}", after_squeeze);
+        let after_squeeze = start.elapsed();
+        println!("Squeezing challenges took: {:?}", after_squeeze);
 
-        // let start = Instant::now();
+        let start = Instant::now();
         // compute unpadded MLEs
         let unpadded_z1_mles = compute_z1_mles(&m_rq, x, z1_num_vars, &beta, &alpha, &vec_tau);
-        // let after_compute_z1_mles = start.elapsed();
-        // println!(
-        //     "Computing unpadded Z1 MLEs took: {:?}",
-        //     after_compute_z1_mles
-        // );
+        let after_compute_z1_mles = start.elapsed();
+        println!(
+            "Computing unpadded Z1 MLEs took: {:?}",
+            after_compute_z1_mles
+        );
 
         let powers_of_alpha = unpadded_z1_mles[2].evals();
 
-        // let start = Instant::now();
+        let start = Instant::now();
         let (z1_original_claim, z1_final_claim, z1_challenges) = proof
             .z1_sumcheck_proof
             .verify(z1_num_vars, 4, transcript)
             .unwrap();
-        // let after_verify_z1 = start.elapsed();
-        // println!("Verifying Z1 took: {:?}", after_verify_z1);
+        let after_verify_z1 = start.elapsed();
+        println!("Verifying Z1 took: {:?}", after_verify_z1);
 
-        // let start = Instant::now();
+        let start = Instant::now();
         let eq_eval = evaluate_at_challenges(&unpadded_z1_mles[0], &z1_challenges[0..m]);
-        // let after_eq_eval = start.elapsed();
-        // println!("Evaluating eq_tau took: {:?}", after_eq_eval);
+        let after_eq_eval = start.elapsed();
+        println!("Evaluating eq_tau took: {:?}", after_eq_eval);
 
-        // let start = Instant::now();
+        let start = Instant::now();
         let x_alpha_eval = evaluate_at_challenges(
             &unpadded_z1_mles[1],
             &z1_challenges[m_rq.height().ilog2() as usize
                 ..m_rq.height().ilog2() as usize + m_rq.width().ilog2() as usize]
                 .to_vec(),
         );
-        // let after_x_alpha_eval = start.elapsed();
-        // println!("Evaluating x_alpha took: {:?}", after_x_alpha_eval);
+        let after_x_alpha_eval = start.elapsed();
+        println!("Evaluating x_alpha took: {:?}", after_x_alpha_eval);
 
-        // let start = Instant::now();
+        let start = Instant::now();
         let z1_ell_eval = evaluate_at_challenges(
             &unpadded_z1_mles[2],
             &z1_challenges[m_rq.height().ilog2() as usize + m_rq.width().ilog2() as usize..],
         );
-        // let after_ell_eval = start.elapsed();
-        // println!("Evaluating ell took: {:?}", after_ell_eval);
+        let after_ell_eval = start.elapsed();
+        println!("Evaluating ell took: {:?}", after_ell_eval);
 
         let mut rng = get_rng();
 
         let whir = Whir::<F>::new(z1_num_vars, &mut rng);
-        // let start = Instant::now();
+        let start = Instant::now();
         whir.verify(
             &proof.m_mle_proof.as_ref().expect("PCS not implemented"),
             &z1_challenges,
         )
         .expect("m_mle proof does not verify");
         let m_eval = proof.m_mle_proof.as_ref().unwrap().claim;
-        // let after_m_eval = start.elapsed();
-        // println!("WHIR verifying m took: {:?}", after_m_eval);
+        let after_m_eval = start.elapsed();
+        println!("WHIR verifying m took: {:?}", after_m_eval);
 
         let z1_eval_at_random_point = eq_eval * m_eval * x_alpha_eval * z1_ell_eval;
 
@@ -176,42 +177,42 @@ where
         // z_3 verification
         let z3_num_vars = z1_num_vars - m_rq.width().ilog2() as usize;
 
-        // let start = Instant::now();
+        let start = Instant::now();
         let (z3_original_claim, z3_final_claim, z3_challenges) = proof
             .z3_sumcheck_proof
             .verify(z3_num_vars, 3, transcript)
             .unwrap();
-        // let after_verify_z3 = start.elapsed();
-        // println!("Verifying Z3 took: {:?}", after_verify_z3);
+        let after_verify_z3 = start.elapsed();
+        println!("Verifying Z3 took: {:?}", after_verify_z3);
 
-        // let start = Instant::now();
+        let start = Instant::now();
         let z3_ell_eval = evaluate_at_challenges(
             &unpadded_z1_mles[2],
             &z3_challenges[m_rq.height().ilog2() as usize..].to_vec(),
         );
-        // let after_ell_eval_z3 = start.elapsed();
-        // println!("Evaluating ell for Z3 took: {:?}", after_ell_eval_z3);
+        let after_ell_eval_z3 = start.elapsed();
+        println!("Evaluating ell for Z3 took: {:?}", after_ell_eval_z3);
 
         let r0_eval = proof.r0_mle_proof.as_ref().unwrap().claim;
         let r1_eval = proof.r1_mle_proof.as_ref().unwrap().claim;
 
-        // let start = Instant::now();
+        let start = Instant::now();
         let whir = Whir::<F>::new(z3_num_vars, &mut rng);
-        // let after_whir_setup = start.elapsed();
-        // println!("Setting up Whir took: {:?}", after_whir_setup);
-        // let start = Instant::now();
+        let after_whir_setup = start.elapsed();
+        println!("Setting up Whir took: {:?}", after_whir_setup);
+        let start = Instant::now();
         whir.verify(&proof.r0_mle_proof.as_ref().unwrap(), &z3_challenges)
             .expect("r0_mle proof does not verify");
         whir.verify(&proof.r1_mle_proof.as_ref().unwrap(), &z3_challenges)
             .expect("r1_mle proof does not verify");
-        // let after_whir_verify = start.elapsed();
-        // println!("WHIR verifying r0 and r1 took: {:?}", after_whir_verify);
+        let after_whir_verify = start.elapsed();
+        println!("WHIR verifying r0 and r1 took: {:?}", after_whir_verify);
 
         // eq_tau eval
-        // let start = Instant::now();
+        let start = Instant::now();
         let eq_eval = evaluate_at_challenges(&unpadded_z1_mles[0], &z3_challenges[0..m]);
-        // let after_eq_eval_z3 = start.elapsed();
-        // println!("Evaluating eq_tau for Z3 took: {:?}", after_eq_eval_z3);
+        let after_eq_eval_z3 = start.elapsed();
+        println!("Evaluating eq_tau for Z3 took: {:?}", after_eq_eval_z3);
 
         let r_eval = beta[0].mul(&r0_eval) + beta[1].mul(&r1_eval);
 
@@ -227,13 +228,13 @@ where
         let alpha_n_plus_one = (alpha * powers_of_alpha[D - 1]) + F::ONE;
         let alpha_factor = alpha_n_plus_one;
 
-        // let start = Instant::now();
+        let start = Instant::now();
         let mut eq_tau_mle_evals = Vec::<F>::with_capacity(1 << z2_num_vars);
         build_eq_poly(&vec_tau[0..z2_num_vars], &mut eq_tau_mle_evals);
-        // let after_build_eq = start.elapsed();
-        // println!("Building eq_tau for Z2 took: {:?}", after_build_eq);
+        let after_build_eq = start.elapsed();
+        println!("Building eq_tau for Z2 took: {:?}", after_build_eq);
 
-        // let start = Instant::now();
+        let start = Instant::now();
 
         let y_alpha_mle_evals = y_batched
             .chunks(D)
@@ -246,17 +247,17 @@ where
             })
             .collect::<Vec<F>>();
 
-        // let after_y_alpha = start.elapsed();
-        // println!("Computing y_alpha evaluations took: {:?}", after_y_alpha);
+        let after_y_alpha = start.elapsed();
+        println!("Computing y_alpha evaluations took: {:?}", after_y_alpha);
 
-        // let start = Instant::now();
+        let start = Instant::now();
         let z_2 = y_alpha_mle_evals
             .iter()
             .zip(eq_tau_mle_evals.iter())
             .map(|(a, b)| *a * *b)
             .sum::<F>();
-        // let after_z2 = start.elapsed();
-        // println!("Evaluating Z2 took: {:?}", after_z2);
+        let after_z2 = start.elapsed();
+        println!("Evaluating Z2 took: {:?}", after_z2);
 
         assert_eq!(
             z1_original_claim - z_2 - (alpha_factor * z3_original_claim),
